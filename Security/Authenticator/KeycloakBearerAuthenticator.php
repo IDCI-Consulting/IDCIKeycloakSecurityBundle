4b3c4b7b012c966c64165acdf15373f5f1e8e4fa
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2019-05-14 14:45:49 +0200

    Fix: missing namespace

--- Security/Authenticator/KeycloakBearerAuthenticator.php
+++ Security/Authenticator/KeycloakBearerAuthenticator.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
